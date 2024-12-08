6466970cdd3bbd7b766b8f1a467d5df3a2a49eb7
Ghifarinugraha <ghifatycoon@gmail.com>
2024-12-09 00:54:49 +0700

    add swiper card

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,37 @@
 import './bootstrap';
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+//import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    //Pagination
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    breakpoints: {
+        0: {
+            slidesPerView:1
+        },
+        620: {
+            slidesPerView:2
+        },
+        1024: {
+            slidesPerView:3
+        },
+    },
+});
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,7 +34,7 @@
                 <div class="mt-8 ">
                     <h2 class="text-2xl font-bold">Ruangan yang tersedia</h2>
                 </div>
-                <div class="container">
+                {{-- <div class="container">
                     <div class="swiper">
                         <div class="swiper-wrapper">
                             <div class="mx-auto rounded-lg bg-base-100 w-72 shadow-lg my-4">
@@ -54,7 +54,92 @@
                            
                         </div>
                     </div>
-                </div >
+                </div > --}}
+
+                <!-- Slider main container -->
+        <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+                <div class="mx-auto rounded-lg bg-base-100 w-72 shadow-lg">
+                    <figure>
+                        <div class="relative pb-2/3 bg-red">
+                            <img class=" rounded-lg absolute w-full h-full object-cover" src="{{asset('images/check-icon.svg')}}" alt="Ruang Kelas" />
+                        </div>
+                    </figure>
+                    <div class="p-4">
+                        <h2 class="font-semibold text-lg">Ruang Kelas</h2>
+                        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <button class="mt-2 px-5 py-2 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out"><a href="">Booking</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="mx-auto rounded-lg bg-base-100 w-72 shadow-lg">
+                    <figure>
+                        <div class="relative pb-2/3 bg-red">
+                            <img class=" rounded-lg absolute w-full h-full object-cover" src="{{asset('images/check-icon.svg')}}" alt="Ruang Kelas" />
+                        </div>
+                    </figure>
+                    <div class="p-4">
+                        <h2 class="font-semibold text-lg">Ruang Kelas</h2>
+                        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <button class="mt-2 px-5 py-2 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out"><a href="">Booking</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="mx-auto rounded-lg bg-base-100 w-72 shadow-lg">
+                    <figure>
+                        <div class="relative pb-2/3 bg-red">
+                            <img class=" rounded-lg absolute w-full h-full object-cover" src="{{asset('images/check-icon.svg')}}" alt="Ruang Kelas" />
+                        </div>
+                    </figure>
+                    <div class="p-4">
+                        <h2 class="font-semibold text-lg">Ruang Kelas</h2>
+                        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <button class="mt-2 px-5 py-2 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out"><a href="">Booking</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="mx-auto rounded-lg bg-base-100 w-72 shadow-lg">
+                    <figure>
+                        <div class="relative pb-2/3 bg-red">
+                            <img class=" rounded-lg absolute w-full h-full object-cover" src="{{asset('images/check-icon.svg')}}" alt="Ruang Kelas" />
+                        </div>
+                    </figure>
+                    <div class="p-4">
+                        <h2 class="font-semibold text-lg">Ruang Kelas</h2>
+                        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <button class="mt-2 px-5 py-2 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out"><a href="">Booking</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev absolute top-1/2 z-10 p-2">
+            <div class="border p-1 rounded-full text-gray-900 bg-white/75">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                </svg>
+            </div>
+        </div>
+        <div class="swiper-button-next absolute top-1/2 right-0 z-10 p-2">
+            <div class="border p-1 rounded-full text-gray-900 bg-white/75">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+            </div>
+        </div>
+
+        <!-- If we need scrollbar -->
+        <div class="swiper-scrollbar"></div>
+        </div>
     </x-bg-card>
     </section>
    
