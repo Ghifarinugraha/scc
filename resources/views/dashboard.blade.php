2133af2e884716d77f554ec7932a7b7043e79822
Ghifarinugraha <ghifatycoon@gmail.com>
2024-12-08 23:09:38 +0700

    fixed card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,6 +30,31 @@
                        </div>
                    </div>    
                 </div>
+
+                <div class="mt-8 ">
+                    <h2 class="text-2xl font-bold">Ruangan yang tersedia</h2>
+                </div>
+                <div class="container">
+                    <div class="swiper">
+                        <div class="swiper-wrapper">
+                            <div class="mx-auto rounded-lg bg-base-100 w-72 shadow-lg my-4">
+                                <figure>
+                                    <div class="relative pb-2/3 bg-red">
+                                        <img class=" rounded-lg absolute w-full h-full object-cover"
+                                        src="{{asset('images/check-icon.svg')}}"
+                                        alt="Ruang Kelas" />
+                                    </div>
+                                </figure>
+                                <div class="p-4">
+                                    <h2 class="font-semibold text-lg">Ruang Kelas</h2>
+                                    <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                    <button class="mt-2 px-5 py-2 rounded-lg  text-white font-semibold bg-orange-400 hover:bg-orange-500 transition ease-in-out"><a href="">Booking</a></button>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div >
     </x-bg-card>
     </section>
    
